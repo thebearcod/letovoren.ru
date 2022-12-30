ded5b81ec3e5c2329462497b646f5a6d62c4ef20
Kostenko Maksim <kostenko.maksim@gmail.com>
2022-12-30 18:13:38 +0500

    add 404.php

---
+++ html/404.php
@@ -0,0 +1,58 @@
+<?php
+header("HTTP/1.1 404 Not Found");
+?>
+<!DOCTYPE HTML>
+<html lang="ru">
+	<head>
+		<title>Моментальный загар в Оренбурге</title>
+		<link rel="shortcut icon" type="image/x-icon" href="/assets/css/images/star-half.png"/>
+		<meta name="yandex-verification" content="7bcea9b7fd971669" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Моментальный загар в Оренбурге от 800 руб - Профессионально" />
+		<meta name="keywords" content="моментальный загар,моментальный загар отзывы, моментальный загар оренбург,моментальный загар держится, моментальный загар обучение,сколько держится моментальный загар,моментальный загар фото +до +и после,моментальный загар цена,моментальный загар +на дому,загар,bronzeglow,летоворен,letovoren,студия моментального загара,студия загара" />
+		<!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
+		<script src="/assets/js/skel.min.js"></script>
+		<script src="/assets/js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="/assets/css/skel.css" />
+			<link rel="stylesheet" href="/assets/css/style.css" />
+			<link rel="stylesheet" href="/assets/css/style-wide.css" />
+			<link rel="stylesheet" href="/assets/css/style-noscript.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie/v8.css" /><![endif]-->
+	</head>
+	<body class="loading">
+		<div id="wrapper">
+
+			<div id="bg2"></div>
+
+			<!--<div id="overlay"></div>-->
+			<div id="main">
+
+				<!--<div id="sidebar-up">
+					<img src="assets/css/images/star-half2.png" alt="letovoren" class="logohead">
+					<p class="contacts"><i class="fa fa-phone" aria-hidden="true"></i><p>
+					<p class="contacts h1"> +7 (967) 777-00-96 </p>-->
+
+
+					<!--<div id="sidebar-up-right">
+						<p> (967) 777-00-96 <i class="fa fa-phone" aria-hidden="true"></i></p>
+					</div>
+				</div>-->
+
+
+				<!-- Header -->
+					<header id="header">
+						<h1 class="logo"> 404 Страница не найдена</h1>
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<span class="copyright">2007-2021 &copy; Студия моментального загара LETOVOREN <a href="#" target="_blank">zagar@letovoren.ru</a></span>
+					</footer>
+
+			</div>
+		</div>
+	</body>
+</html>
